6dddba5fc648a219678e071d4db084ee533bf9b7
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-12-10 23:29:44 +0200

    Getting a parsed url as well as path, taking exact path name and trimming it to remove slashes. Getting the method of request. Getting query string as an object. Getting headers as an object. Using StringDecoder to get the payload if any, placing payload stream by appending bit by bit for buffer and then ending decoder.

--- index.js
+++ index.js
@@ -5,10 +5,45 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello world!\n');
+
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello world!\n');
+
+    // Log the request path
+    console.log(`Payload is: ${buffer}`);
+  });
 });
 
 // Start the server, and have it listen on port 3000
