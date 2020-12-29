18c97a7432385434bd90ccb75ae5db99fce0c96d
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-12-29 22:25:08 +0200

    Added Fetch system for frontend to work when going to differnt api locations. Added accountCreate html page, without functionality yet.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,67 @@
-const app = {};
+const app = {
+  config: {
+    sessionToken: false
+  },
+  client: {
+    request(headers, path, method, queryStringObject, payload, callback) {
+      // Setting defaults
+      headers = typeof headers === 'object' && headers !== null ? headers : {};
+      path = typeof path === 'string' ? path : '/';
+      method =
+        typeof method === 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method.toUpperCase()) > -1
+          ? method.toUpperCase()
+          : 'GET';
+      queryStringObject = typeof queryStringObject === 'object' && queryStringObject !== null ? queryStringObject : {};
+      payload = typeof payload === 'object' && payload !== null ? payload : {};
+      callback = typeof callback === 'function' ? callback : false;
 
-console.log('Hello console world!');
+      const requestUrl = `${path}?`;
+      let counter = 0;
+
+      for (let queryKey in queryStringObject) {
+        if (queryStringObject.hasOwnProperty(queryKey)) {
+          counter++;
+
+          if (counter > 1) {
+            requestUrl += '&';
+          }
+
+          requestUrl += `${queryKey}=${queryStringObject[queryKey]}`;
+        }
+      }
+
+      let headersPreparedForFetch = new Headers();
+      headersPreparedForFetch.append('Content-Type', 'application/json');
+
+      if (app.config.sessionToken) {
+        headersPreparedForFetch.append('token', app.config.sessionToken.id);
+      }
+
+      for (let headerKey in headers) {
+        headersPreparedForFetch.append(headerKey, headers[headerKey]);
+      }
+
+      fetch(requestUrl, {
+        method: method,
+        headers: headersPreparedForFetch
+      })
+        .then((response) => {
+          return response.json().then((data) => {
+            return {
+              status: response.status,
+              body: data
+            };
+          });
+        })
+        .then((response) => {
+          if (callback) {
+            try {
+              callback(response.status, response.body);
+            } catch (err) {
+              callback(response.status, false);
+            }
+          }
+        });
+    }
+  }
+};
