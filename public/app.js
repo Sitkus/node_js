b40a70636603cef3f9ebca9c2be04e479e53c85f
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-12-30 22:42:14 +0200

    Trying to add functionality for account creation to work, but got stuck.

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ const app = {
       payload = typeof payload === 'object' && payload !== null ? payload : {};
       callback = typeof callback === 'function' ? callback : false;
 
-      const requestUrl = `${path}?`;
+      let requestUrl = `${path}?`;
       let counter = 0;
 
       for (let queryKey in queryStringObject) {
@@ -30,8 +30,10 @@ const app = {
         }
       }
 
-      let headersPreparedForFetch = new Headers();
-      headersPreparedForFetch.append('Content-Type', 'application/json');
+      let headersPreparedForFetch = new Headers({
+        'Content-Type': 'application/json'
+      });
+      // headersPreparedForFetch.append('Content-Type', 'application/json');
 
       if (app.config.sessionToken) {
         headersPreparedForFetch.append('token', app.config.sessionToken.id);
@@ -41,12 +43,16 @@ const app = {
         headersPreparedForFetch.append(headerKey, headers[headerKey]);
       }
 
+      // console.log(requestUrl, method, headersPreparedForFetch);
+
       fetch(requestUrl, {
         method: method,
         headers: headersPreparedForFetch
       })
         .then((response) => {
+          console.log(response);
           return response.json().then((data) => {
+            console.log(data);
             return {
               status: response.status,
               body: data
@@ -54,6 +60,7 @@ const app = {
           });
         })
         .then((response) => {
+          console.log(response);
           if (callback) {
             try {
               callback(response.status, response.body);
@@ -63,5 +70,60 @@ const app = {
           }
         });
     }
+  },
+  bindForms() {
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const selectedForm = document.querySelector('form');
+
+      const formId = selectedForm.getAttribute('id');
+      const path = selectedForm.getAttribute('action');
+      const method = selectedForm.getAttribute('method').toUpperCase();
+
+      document.querySelector(`#${formId} .form__error`).style.display = 'hidden';
+
+      let payload = {};
+      const elements = selectedForm.elements;
+      console.log(elements);
+
+      for (let i = 0; i < elements.length; i++) {
+        if (elements[i].nodeName !== 'BUTTON') {
+          const valueOfElement =
+            elements[i].attributes.type.value === 'checkbox' ? elements[i].checked : elements[i].value;
+
+          payload[elements[i].name] = valueOfElement;
+        }
+      }
+      console.log(payload);
+
+      app.client.request(undefined, path, method, undefined, payload, (statusCode, responsePayload) => {
+        if (statusCode !== 200) {
+          const error =
+            typeof responsePayload.Error === 'string' ? responsePayload.Error : 'An error occured, please try again';
+
+          document.querySelector(`#${formId} .form__error`).innerHTML = error;
+
+          document.querySelector(`#${formId} .form__error`).style.display = 'block';
+        } else {
+          app.formResponseProcessor(formId, payload, responsePayload);
+        }
+      });
+    });
+  },
+  formResponseProcessor(formId, requestPayload, responsePayload) {
+    const functionToCall = false;
+
+    if (formId === 'form--create') {
+      // @TODO Do something here now that the account has been created successfully
+      console.log('The account create form was successfully submitted');
+    }
+  },
+  init() {
+    app.bindForms();
   }
+};
+
+window.onload = () => {
+  app.init();
 };
